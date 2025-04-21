761fe3bec76bb8fa39aec9b594dea1cea5b303c6
SHravankumar Muchchandi <shravankumarmuchchandi@gmail.com>
2025-04-21 16:46:43 +0530

    21-04-2025

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ admin.initializeApp({
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI,{
+    dbName: "MindSync",
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
